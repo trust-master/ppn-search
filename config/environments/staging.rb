1a826d116623437422d3357124cad412df165e90
Ryan Taylor Long <ryan@rtlong.com>
2012-01-09 04:39:58 -0600

    Make sure Whereuat assets get compiled

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -45,7 +45,7 @@ ServiceProviderPortal::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( ie8.css ie7.css )
+  config.assets.precompile += %w( ie8.css ie7.css whereuat.js whereuat.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
