f78ae0b701db590796f5649c8fb20c8d5d6cae13
Ryan Taylor Long <ryan@rtlong.com>
2012-03-21 22:59:37 -0500

    Get tests passing again

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   validates             :email, presence: true, uniqueness: true, email: true
   validates_presence_of :first_name, :last_name, :role, :company_id
   validates_presence_of :password, :password_confirmation, on: :create
-  validates             :password, password: true, on: :create
+  validates             :password, password: true, allow_nil: true
 
   attr_accessible :first_name, :middle_name, :last_name, :email, :password, :password_confirmation, as: [:default, :admin]
   attr_accessible :role, :company_id, :active, as: [:admin]
