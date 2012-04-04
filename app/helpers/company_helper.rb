466843634284ad2cfa87fa0acb6d6665920188f3
Ryan Taylor Long <ryan@rtlong.com>
2012-04-03 20:33:15 -0500

    Differently style inactive Providers shown to Admins in search results
    [#21709733] *Admin* can search for *all* companies, optionally filtering by (sub-)category

--- app/helpers/company_helper.rb
+++ app/helpers/company_helper.rb
@@ -3,5 +3,10 @@ module CompanyHelper
     { false => t('service_areas.partial_only.false'),
       true =>  t('service_areas.partial_only.true') }.to_a
   end
+
+  def dom_class_for_search_result(company)
+    dom_class = []
+    dom_class << :inactive unless company.active && company.visible
+  end
 end
 
