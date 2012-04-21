7e4eb2ff40bb2a2e4d2e0b14fda057d126552d58
Ryan Taylor Long <ryan@rtlong.com>
2012-04-21 18:06:15 -0500

    Add Helvetica and design preview page

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -1,6 +1,9 @@
 class DebugController < ApplicationController
   authorize_resource :admin
 
+  # This is exactly the same as rake routes, in the browser. Handy when you've got a development
+  # server running already and you don't want to wait 3 minutes for the app to initialize for the
+  # rake task.
   def routes
     Rails.application.reload_routes!
     all_routes = Rails.application.routes.routes
@@ -9,6 +12,15 @@ class DebugController < ApplicationController
     inspector = Rails::Application::RouteInspector.new
 
     @routes = inspector.collect_routes(all_routes)
+  end
+
+
+  def design
+    # @colors = Sass::Engine.for_file(Rails.root.join('app/assets/stylesheets/_colors.sass'))
+    @fonts = []
+    engine = Sass::Engine.for_file(Rails.root.join('app/assets/stylesheets/_fonts.sass').to_s, {})
+    engine.to_tree.children.each { |n| @fonts << [n.name, n.expr.to_s] if n.is_a?(Sass::Tree::VariableNode) }
+
   end
 
 end
