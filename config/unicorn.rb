98930ce04d08338216ed4ac133db3334790b1579
Ryan Taylor Long <ryan@rtlong.com>
2012-04-26 20:44:00 -0500

    Fix issue with NewRelic and Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
 
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
+
+# load the app into memory before forking. Important, as the NewRelic agent needs to fully start up,
+# and Improperly deployed applications can go into a spawn loop if the application fails to load
+preload_app true
